a6c926a9ffcbc2d59d2ab4678b46c376c598efca
WilsonRusli <wilsonrusli066@gmail.com>
2025-01-12 00:03:57 +0700

    Update input prompts and display all data from database.csv

--- main.py
+++ main.py
@@ -3,9 +3,15 @@ import streamlit as st
 st.title("Son Application")
 st.write("Masukkan nama brand")
 
-brand = st.text_input("Masukkan text: ")
+brand = st.text_input("Masukkan nama anda")
 cam = st.camera_input("Say cheese")
 t = st.color_picker("Hello")
+
+# print all data
+with open("database.csv", "r") as f:
+    data = f.readlines()
+
+st.write(data)
 
 if brand:
     st.write("Teks anda adalah")
@@ -14,11 +20,7 @@ if brand:
     with open("database.csv", "a") as f:
         f.write(brand + "\n")
     
-    # print all data
-    with open("database.csv", "r") as f:
-        data = f.readlines()
 
-    st.write(data)
     
 
     st.write("Created by felix")
