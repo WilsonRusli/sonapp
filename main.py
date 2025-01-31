9eb4910a8c0c059c6f48d349ff1716afa922b37d
WilsonRusli <wilsonrusli066@gmail.com>
2025-01-31 19:56:24 +0700

    Add debug statement to display Genius API key in main.py

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ load_dotenv(dotenv_path='geniusAPI.env')
 
 # Get the Genius API key from environment variables
 GENIUS_API_KEY = os.getenv('client_access_token')
+st.write(GENIUS_API_KEY)
 if GENIUS_API_KEY is None:
     st.error("Genius API key not found. Please set the 'client_access_token' in the environment variables.")
 
