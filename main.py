3580e7c800ed760c9743181c1bc71e08b401a5e4
WilsonRusli <wilsonrusli066@gmail.com>
2025-02-01 21:17:10 +0700

    Remove error handling for missing Genius API key from main.py

--- main.py
+++ main.py
@@ -16,8 +16,6 @@ load_dotenv(dotenv_path='geniusAPI.env')
 # Get the Genius API key from environment variables
 GENIUS_API_KEY = os.getenv("GENIUS_API_KEY")
 
-if GENIUS_API_KEY is None:
-    st.error("Genius API key not found. Please set the 'client_access_token' in the environment variables.")
 
 
 # Set path ke binary Tesseract
